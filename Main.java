ab127ef8c7f91be1135a2ba46c8d6a7c379cb55b
Anatoly Moskalev <moskalev1997tolya@gmail.com>
2021-10-28 13:42:53 +0600

    InicialCommit

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 30_000;
+        int bonusMileValue = 20;
+        int bonusAmount = ticketPrice / bonusMileValue;
+        System.out.println(bonusAmount);
+
+
+
+    }
+}
